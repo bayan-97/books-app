37d24ea3443daf1972792db10b0217d368e32177
bayanalalem <bayanalalem0@gmail.com>
2020-09-07 19:42:27 +0300

    done

---
+++ sechma.sql
@@ -0,0 +1,9 @@
+DROP TABLE books;
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  isbn VARCHAR(255),
+  image_url VARCHAR(255),
+  description TEXT
+);
